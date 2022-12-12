fc3641fd602374ce462e6be820d48a85da22b7bd
Burak Aslandogdu <aslandogduburak@gmail.com>
2022-12-12 13:59:32 +0300

    proje devam

--- navigation/Screens/Login/Login.js
+++ navigation/Screens/Login/Login.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import {SafeAreaView, Text, View, Image} from 'react-native';
+import {SafeAreaView, Text, View, Image, ScrollView} from 'react-native';
 import Button from '../components/Button/Button';
 import Input from '../components/Input/Input';
 import styles from './Login.style';
+import auth from '@react-native-firebase/auth';
 //static şeyler için require tercih edilir.
 //urller bir enter point'ten çekiyorsan kullanılır
 const Login = () => {
+  // const signUp = () => {
+  //   auth()
+  //     .createUserWithEmailAndPassword(
+  //       'jane.doe@example.com',
+  //       'SuperSecretPassword!',
+  //     )
+  //     .then(res => console.log(res))
+  //     .catch(err => console.log(err));
+  // };
+
+  // const signIn = () => {
+  //   auth()
+  //     .signInWithEmailAndPassword(
+  //       'jane.doe@example.com',
+  //       'SuperSecretPassword!',
+  //     )
+  //     .then(res => console.log('Signed in.'))
+  //     .catch(err => console.log(err));
+  // };
+
+  // const signOut = () => {
+  //   auth()
+  //     .signOut()
+  //     .then(res => console.log('Signed out.'))
+  //     .catch(err => console.log(err));
+  // };
+  // const checkOut = () => {
+  //   const user = auth().currentUser;
+  //   console.log(user);
+  // };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.logo_container}>
@@ -18,6 +49,11 @@ const Login = () => {
         <Input placeholder="Kullanıcı Adını Giriniz." />
         <Input placeholder="Şifrenizi Giriniz." />
         <Button text="Giriş Yap" />
+        <Button text="Kayıt Ol" />
+        {/* <Button text="Sign Up" onPress={signUp} />
+          <Button text="Sign In" onPress={signIn} />
+          <Button text="Sign Out" onPress={signOut} />
+          <Button text="Check User" onPress={checkOut} /> */}
       </View>
     </SafeAreaView>
   );
